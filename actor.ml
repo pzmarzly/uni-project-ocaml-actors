f0aabd815aef77a212ecf613acfd88f914641934
pzmarzly <pawo2500@gmail.com>
2022-01-17 00:41:41 +0100

    fix calls

--- actor.ml
+++ actor.ml
@@ -3,60 +3,68 @@ type serializer =
 | SInt
 | SList of serializer
 
-type 'data cast = 'data -> 'data
-type ('data, 'ret) call = 'data -> 'data * 'ret
-
 module type Actor = sig
   type data
   val data_format : (string * serializer) list
   val default : data
 end
 
-module Executor : sig
+module rec Executor : sig
   type 'data pid
+  type 'a task
+  type t
+
   val make_pid : (module Actor with type data = 'data) -> ('data pid -> 'a) -> 'a
 
-  type 'a task
   val return_task : 'a -> 'a task
-  val enqueue_call : 'data pid -> ('data, 'ret) call -> ('ret -> 'a task) -> 'a task
+  val enqueue_call : 'data pid -> ('data, 'ret) T.call -> ('ret -> 'a task) -> 'a task
 
-  type t
   val new_executor : unit -> t
   val add_task : t -> unit task -> unit
   val run_tasks : t -> unit
 end = struct
-  type 'a pid = 'a ref * (unit -> unit) list ref
+  (* instancja aktora: wewnętrzny stan i lista obliczeń do wykonania *)
+  type 'a pid = 'a ref * unit M.t list ref
+  (* stan zadania do wykonania/wykonanego na executorze *)
+  and 'a task =
+  | SentTo of unit M.t list ref
+  | Finished of 'a
+  | Enqueued of (unit -> 'a task)
+  (* stan executora: kolejka zadań *)
+  and t = unit task Queue.t
+
   let make_pid (type data) (module M : Actor with type data = data) k =
     k (ref M.default, ref [])
 
-  type 'a task =
-  | SentTo of (unit -> unit) list ref
-  | Finished of 'a
-  | Enqueued of (unit -> 'a task)
   let return_task x = Finished x
 
-  let enqueue_call pid fn k =
+  let enqueue_call (pid : 'data pid) (fn : ('data, 'ret) T.call) (k : 'ret -> 'a task) : 'a task =
     Enqueued (fun () ->
       let data, queue = pid in
-      let task = (fun () ->
-        let new_data, ret = fn !data in
-        data := new_data;
-        let _ = k ret in ()
-      ) in
+      let task : unit M.t =
+        M.bind
+          (M.flatten (M.return_lazy (fun () -> fn !data)))
+          (fun (new_data, ret) ->
+            data := new_data;
+            let _ = k ret in
+            M.return ())
+      in
       queue := task :: !queue;
       SentTo queue)
 
-  type t = unit task Queue.t
-  let new_executor = Queue.create
+  let new_executor () : unit task Queue.t = Queue.create ()
   let add_task t task = Queue.push task t
-  let rec drain_queue_task queue =
+  let rec make_task_for_actor (queue : unit M.t list ref) t : unit task =
     Enqueued (fun () ->
       match !queue with
       | [] -> Finished ()
-      | x :: xs -> x (); queue := xs; drain_queue_task queue)
+      | x :: xs ->
+        Queue.push (x |> M.into_task) t;
+        queue := xs;
+        make_task_for_actor queue t)
   let rec run_task t task =
     match task with
-    | SentTo queue -> Queue.push (drain_queue_task queue) t
+    | SentTo queue -> Queue.push (make_task_for_actor queue t) t
     | Finished value -> ()
     | Enqueued fn -> run_task t (fn ())
   let run_tasks t =
@@ -65,22 +73,26 @@ end = struct
     done
 end
 
-module QueueMonad : sig
+and M : sig
   type 'a t
   type 'data pid
 
   val return : 'a -> 'a t
+  val return_lazy : (unit -> 'a) -> 'a t
   val bind : 'a t -> ('a -> 'b t) -> 'b t
+  val flatten : 'a t t -> 'a t
 
   val spawn : (module Actor with type data = 'data) -> 'data pid t
-  val call : 'data pid -> ('data, 'a) call -> 'a t
+  val call : 'data pid -> ('data, 'a) T.call -> 'a t
   val into_task : 'a t -> 'a Executor.task
 end = struct
   type 'a t = { run : 'r. ('a -> 'r Executor.task) -> 'r Executor.task }
   type 'data pid = 'data Executor.pid
 
   let return x = { run = fun k -> k x }
+  let return_lazy f = { run = fun k -> k (f ()) }
   let bind a f = { run = fun k -> a.run (fun x -> (f x).run k ) }
+  let flatten x = bind x (fun y -> y)
 
   let spawn (type data) (module M : Actor with type data = data) =
     { run = fun k -> Executor.make_pid (module M) k }
@@ -90,4 +102,12 @@ end = struct
 
   let into_task t =
     t.run Executor.return_task
+end
+
+and T : sig
+  type 'data cast = 'data -> 'data M.t
+  type ('data, 'ret) call = 'data -> ('data * 'ret) M.t
+end = struct
+  type 'data cast = 'data -> 'data M.t
+  type ('data, 'ret) call = 'data -> ('data * 'ret) M.t
 end
--- counter.ml
+++ counter.ml
@@ -3,45 +3,45 @@ open Actor
 module Counter
 : sig
   include Actor
-  val increase : data cast
-  val set : int -> (data, int) call
+  val increase : data T.cast
+  val set : int -> (data, int) T.call
 end
 = struct
   type data = int
   let data_format = [("value", SInt)]
   let default = 0
-  let increase v = v + 1
-  let set new_state state = new_state, state
+  let increase v = M.return (v + 1)
+  let set new_state state = M.return (new_state, state)
 end
 
 module Counters
 : sig
   include Actor
-  val increase : data cast
-  val get_current : (data, int) call
-  val get_total : (data, int) call
-  val set_current : int -> (data, int) call
+  val increase : data T.cast
+  val get_current : (data, int) T.call
+  val get_total : (data, int) T.call
+  val set_current : int -> (data, int) T.call
 end
 = struct
   type data = int * int
   let data_format = [("current", SInt); ("total", SInt)]
 
   let default = (0, 0)
-  let increase (state1, state2) = (state1 + 1, state2 + 1)
-  let get_current (state1, state2) = (state1, state2), state1
-  let get_total (state1, state2) = (state1, state2), state2
-  let set_current new_state1 (state1, state2) = (new_state1, state2), state1
+  let increase (state1, state2) = M.return (state1 + 1, state2 + 1)
+  let get_current (state1, state2) = M.return ((state1, state2), state1)
+  let get_total (state1, state2) = M.return ((state1, state2), state2)
+  let set_current new_state1 (state1, state2) = M.return ((new_state1, state2), state1)
 end
 
 let main =
-  let (let* ) = QueueMonad.bind in
-  let* pid = QueueMonad.spawn (module Counters) in
-  let* current = QueueMonad.call pid Counters.get_current in
-  QueueMonad.return (Printf.printf "%i\n" current)
+  let (let* ) = M.bind in
+  let* pid = M.spawn (module Counters) in
+  let* current = M.call pid Counters.get_current in
+  M.return (Printf.printf "%i\n" current)
 
 let run () =
   let ex = Executor.new_executor () in
-  Executor.add_task ex (QueueMonad.into_task main);
+  Executor.add_task ex (M.into_task main);
   Executor.run_tasks ex
 
 (* pomysły: *)
