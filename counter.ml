7d77d8c6cbdffcbaae0bc3084c2b8ef2b99a06a4
pzmarzly <pawo2500@gmail.com>
2022-01-16 22:23:31 +0100

    clean up

--- counter.ml
+++ counter.ml
@@ -19,18 +19,6 @@ end
   let set_current new_state1 (state1, state2) = (new_state1, state2), state1
 end
 
-(* open Impl_id
-let main_id =
-  let (let* ) = IdMonad.bind in
-  let* pid = IdMonad.spawn (module Counter) in
-  let* current = IdMonad.call pid Counter.get_current in
-  IdMonad.return (Printf.printf "%i\n" current)
-open Impl_cps
-let main_cps =
-  let (let* ) = CpsMonad.bind in
-  let* pid = CpsMonad.spawn (module Counter) in
-  let* current = CpsMonad.call pid Counter.get_current in
-  CpsMonad.return (Printf.printf "%i\n" current) *)
 open Executor
 let main =
   let (let* ) = QueueMonad.bind in
