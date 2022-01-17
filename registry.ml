7dc4b81c0f3f77825d5dfc5c27fa07e6a73c8eb3
pzmarzly <pawo2500@gmail.com>
2022-01-18 00:06:52 +0100

    add registry example

---
+++ registry.ml
@@ -0,0 +1,42 @@
+open Actor
+open Actor.M
+let (let* ) = bind
+
+module Registry (Impl : Actor) : sig
+  include (module type of Impl)
+  val get : unit -> Impl.data M.pid
+  val set : Impl.data M.pid -> unit
+end = struct
+  include Impl
+  let instance = ref (M.spawn (module Impl))
+  let get () = !instance
+  let set v = instance := v
+end
+
+module Counter
+: sig
+  include Actor
+  val increase : data cast
+  val get : (data, int) call
+  val set : int -> (data, int) call
+end
+= struct
+  type data = int
+  let data_format = [("value", SInt)]
+  let default () = 0
+  let increase v = M.return (v + 1)
+  let get v = M.return (v, v)
+  let set new_state state = M.return (new_state, state)
+end
+
+module CounterInstance = Registry(Counter)
+
+let main =
+  let* () = cast (CounterInstance.get ()) Counter.increase in
+  let* current = call (CounterInstance.get ()) Counter.get in
+  return (Printf.printf "%i\n" current)
+
+let run () =
+  let ex = Executor.new_executor () in
+  Executor.add_task ex main;
+  Executor.run_tasks ex
